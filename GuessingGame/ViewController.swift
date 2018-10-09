dbc2379d0bfae85e36dcc3e06c5e6d460ef329ed
Ezra Kiteck <ezrakiteck@interapts-macbook-pro-46.local>
2018-10-09 10:22:31 -0500

    Initial Commit

---
+++ GuessingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuessingGame
+//
+//  Created by Ezra Kiteck on 10/9/18.
+//  Copyright © 2018 Ezra Kiteck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
